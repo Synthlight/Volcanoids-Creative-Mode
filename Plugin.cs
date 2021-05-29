7da7fe8a851a280f9088d499ae2049b2bd8f7285
Gregory Brutsche <gregory@mail.brutsches.local>
2021-05-29 18:13:18 -0400

    Initial commit.

---
+++ Plugin.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Base_Mod;
+using JetBrains.Annotations;
+
+namespace Creative_Mode {
+    [UsedImplicitly]
+    public class EnableDevMode : BaseGameMod {
+        protected override string ModName => "Creative-Mode";
+
+        public override void OnInitData() {
+            var categoryList = (from recipe in GameResources.Instance.Recipes
+                                where recipe.Categories != null && recipe.Categories.Length > 0
+                                from category in recipe.Categories
+                                where !string.IsNullOrEmpty(category.name)
+                                orderby category.name
+                                select category).Distinct()
+                                                .ToList();
+
+            // Production
+            var newProdCategoryList = (from category in categoryList
+                                       where category.name == "CraftingTier1"
+                                             || category.name == "ProductionTier1"
+                                             || category.name == "CraftingTierSubmarine"
+                                             || category.name == "ProductionTierSubmarine"
+                                       select category).ToArray();
+
+            // Research
+            var newResearchCategoryList = (from category in categoryList
+                                           where category.name == "ResearchTier1"
+                                                 || category.name == "CraftingResearchTier1"
+                                           select category).ToArray();
+
+            // Refinery
+            var newRefineryCategoryList = (from category in categoryList
+                                           where category.name == "RefinementTier1"
+                                                 || category.name == "CraftingRefineryTier1"
+                                           select category).ToArray();
+
+            // Scrap
+            var newScrapCategoryList = new[] {categoryList.GetCategory("ScrapTier1")};
+
+            foreach (var recipe in GameResources.Instance.Recipes) {
+                // Hide worktable variations of recipes.
+                if (recipe.name.Contains("WorktableRecipe") && recipe.name != "WorktableRecipe"
+                    // And the x2/x5 variants.
+                    || recipe.name.EndsWith("Recipe2")) {
+                    recipe.Categories = new RecipeCategory[0];
+                    continue;
+                }
+
+                recipe.Inputs           = new InventoryItem[0];
+                recipe.RequiredUpgrades = new ItemDefinition[0];
+                recipe.ProductionTime   = 1f;
+
+                if (recipe.Categories.Contains("CraftingTier") || recipe.Categories.Contains("ProductionTier")) {
+                    recipe.Categories = newProdCategoryList;
+                }
+
+                if (recipe.Categories.Contains("CraftingResearch") || recipe.Categories.Contains("ResearchTier")) {
+                    recipe.Categories = newResearchCategoryList;
+                }
+
+                if (recipe.Categories.Contains("CraftingRefinery") || recipe.Categories.Contains("RefinementTier")) {
+                    recipe.Categories = newRefineryCategoryList;
+                }
+
+                if (recipe.Categories.Contains("ScrapTier")) {
+                    recipe.Categories = newScrapCategoryList;
+                }
+            }
+
+            base.OnInitData();
+        }
+    }
+
+    public static class Extensions {
+        public static bool Contains(this RecipeCategory[] categories, string name, bool partialMatch = true) {
+            if (categories == null || categories.Length == 0) return false;
+            foreach (var category in categories) {
+                switch (partialMatch) {
+                    case true when category.name.Contains(name):
+                    case false when category.name == name:
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static RecipeCategory GetCategory(this IEnumerable<RecipeCategory> categories, string name) {
+            return categories.FirstOrDefault(category => category.name == name);
+        }
+    }
+}
